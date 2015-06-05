0a883857b419085da10eb6ff4031a5e68fd0e1bf
Santeri Räisänen <santerir@cs.helsinki.fi>
2015-06-05 14:14:45 +0300

    initial commit

---
+++ CODE/spectralClustering.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#	IMPORTS
+import rpy2.robjects as ro
+import numpy as np 
+import scipy as sp
+
+#	This is an algorithm for spectral clustering.
+#	It implements kNN utilities from R, but work is bieng done to translate these to run natively on python
+#	
+#
+#
+#
+#
+#
+#
+#
+#	Santeri Räisänen, 2015
+
+
